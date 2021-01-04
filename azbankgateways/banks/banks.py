9b8ccc535691fe2792a689d1fc503734ab839bf3
Ali Zahedigol <ali.zahedigol@gmail.com>
2021-01-04 13:35:05 +0200

    fix: reformat

--- azbankgateways/banks/banks.py
+++ azbankgateways/banks/banks.py
@@ -2,8 +2,8 @@ import abc
 import logging
 import uuid
 from urllib import parse
-
 import six
+
 from django.db.models import Q
 from django.shortcuts import redirect
 from django.utils import timezone
