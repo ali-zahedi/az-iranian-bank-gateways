fce9f864ae0d0ddd2661f0aeaaab2e3fc5e7815d
Ali Zahedigol <alizahedigol@gmail.com>
2025-12-04 09:42:50 +0100

    fix: configure isort to ignore azbankgateways/banks/__init__.py prevent circular import
    linter ignore

--- azbankgateways/banks/__init__.py
+++ azbankgateways/banks/__init__.py
@@ -1,10 +1,35 @@
-from .banks import BaseBank  # noqa
-from .asanpardakht import AsanPardakht  # noqa
-from .bahamta import Bahamta  # noqa
-from .bmi import BMI  # noqa
-from .idpay import IDPay  # noqa
-from .mellat import Mellat  # noqa
-from .payV1 import PayV1  # noqa
-from .sep import SEP  # noqa
-from .zarinpal import Zarinpal  # noqa
-from .zibal import Zibal  # noqa
+"""
+This package exposes bank gateway classes.
+
+NOTE:
+`from .banks import BaseBank` **must appear first** to avoid circular-import
+issues. Other classes depend on `BaseBank`, so importing it earlier prevents
+initialization-order problems.
+"""
+
+# isort: off
+from .banks import BaseBank
+from .asanpardakht import AsanPardakht
+from .bahamta import Bahamta
+from .bmi import BMI
+from .idpay import IDPay
+from .mellat import Mellat
+from .payV1 import PayV1
+from .sep import SEP
+from .zarinpal import Zarinpal
+from .zibal import Zibal
+
+# isort: on
+
+__all__ = [
+    "BaseBank",
+    "AsanPardakht",
+    "Bahamta",
+    "BMI",
+    "IDPay",
+    "Mellat",
+    "PayV1",
+    "SEP",
+    "Zarinpal",
+    "Zibal",
+]
